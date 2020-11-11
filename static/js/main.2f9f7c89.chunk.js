54f184a9e7dc97a8760a14ddedd4edc9d9393046
dasa <dasabot@icloud.com>
2020-11-11 20:34:27 +0200

    Updates

---
+++ static/js/main.2f9f7c89.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpyalantis-test"]=this["webpackJsonpyalantis-test"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var s=n(0),c=(n(2),n(3));n(9);var i=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("span",{children:"hi"})})};Object(c.render)(Object(s.jsx)(i,{}),document.getElementById("root"))},9:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.2f9f7c89.chunk.js.map
